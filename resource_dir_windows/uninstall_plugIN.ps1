9e2f35db4337d31c05f8c7aa767b3c2919bc4be4
wayneliu <wliu@allxon.com>
2024-04-22 16:22:39 +0800

    DMS-5766 remove try-catch in .ps1

--- resource_dir_windows/uninstall_plugIN.ps1
+++ resource_dir_windows/uninstall_plugIN.ps1
@@ -1,17 +1,11 @@
-try {
-  $appName = "plugin-hello"
-  $currentDir = (Get-Location).Path
+$appName = "plugin-hello"
+$currentDir = (Get-Location).Path
 
-  schtasks /end /tn "$appName-service"
-  schtasks /delete /tn "$appName-service" /f
+schtasks /end /tn "$appName-service"
+schtasks /delete /tn "$appName-service" /f
 
-  timeout /t 1
+timeout /t 1
 
-  # Move to $currentDir's parent directory and remove the directory
-  cd ..
-  Remove-Item -Path "$env:ALLXON_PLUGIN_DIR" -Recurse -Force
-} catch { 
-  $_; 
-  exit 1 
-} 
-exit 0
+# Move to $currentDir's parent directory and remove the directory
+cd ..
+Remove-Item -Path "$env:ALLXON_PLUGIN_DIR" -Recurse -Force
